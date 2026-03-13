ca4d0194f89e81ccea84d9efe6ea71cdbaf37506
Roy Honders <hi@royhonders.com>
2026-03-13 17:55:55 +0100

    chore: update .gitignore and remove build artifacts from index

--- .output/server/chunks/build/AppFooter-styles.CiXb6BVg.mjs
+++
@@ -1,8 +0,0 @@
-import { A as AppFooter_vue_vue_type_style_index_0_scoped_8b6a07b0_lang } from './entry-styles-8.mjs-DAKgV84A.mjs';
-
-const AppFooterStyles_CiXb6BVg = [
-  AppFooter_vue_vue_type_style_index_0_scoped_8b6a07b0_lang
-];
-
-export { AppFooterStyles_CiXb6BVg as default };
-//# sourceMappingURL=AppFooter-styles.CiXb6BVg.mjs.map
--- .output/server/chunks/build/entry-styles-9.mjs-hvvPXvQv.mjs
+++
@@ -1,4 +0,0 @@
-const app_vue_vue_type_style_index_0_lang = "";
-
-export { app_vue_vue_type_style_index_0_lang as a };
-//# sourceMappingURL=entry-styles-9.mjs-hvvPXvQv.mjs.map
